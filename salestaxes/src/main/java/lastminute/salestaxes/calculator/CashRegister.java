ff0202171b12c4f9b79388c13052dd8630457ed1
rosariobarresi <r.barresi89@gmail.com>
2017-10-05 19:13:12 +0200

    minor

--- salestaxes/src/main/java/lastminute/salestaxes/calculator/CashRegister.java
+++ salestaxes/src/main/java/lastminute/salestaxes/calculator/CashRegister.java
@@ -1,32 +1,20 @@
 package lastminute.salestaxes.calculator;
 
-import lastminute.salestaxes.bean.Item;
 import lastminute.salestaxes.input.Basket;
 import lastminute.salestaxes.output.Receipt;
 
 public class CashRegister implements ICashRegister {
 
-	@Override
-	public Receipt calculate(Basket basket) {
+	public  Receipt calculate( Basket basket) {
 		Receipt recipt = new Receipt();
 		basket.getListItem().forEach(item -> {
 			recipt.addItem(item);
-			recipt.addToTaxes(calulatePriceTaxes(item, Boolean.FALSE));
-			double priceWithTaxes = calulatePriceTaxes(item, Boolean.TRUE);
-			recipt.addToTotal(priceWithTaxes);
-			item.setPrice(priceWithTaxes);
+			double taxes = Calculator.roundNearBound(Calculator.getTaxesItem(item), 2);
+			recipt.addToTaxes(taxes);
+			recipt.addToTotal(item.getPrice() + taxes);
+			item.setPrice(item.getPrice() + taxes);
 		});
 		return recipt;
-	}
-
-	private double calulatePriceTaxes(Item item, boolean total) {
-		double price = item.getPrice();
-		if (total) {
-			price = Calculator.getPriceItemeWithTaxes(item);
-		} else {
-			price = Calculator.roundNearBound(Calculator.getTaxesItem(item), 2);
-		}
-		return price;
 	}
 
 }
